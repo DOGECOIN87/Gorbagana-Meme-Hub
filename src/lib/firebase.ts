6865f857edbd325d0887d6c2d9462ebcb13baf7d
Matthew Mobley <mattrickbeats@gmail.com>
2025-12-07 01:50:02 +0000

    Skip to main content Firebase logo Project Overview My First Project Hel

---
+++ src/lib/firebase.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpdleXicA9oeiHPdb1FKEaHfC8TSi5gq0",
+  authDomain: "delta-cortex-387523.firebaseapp.com",
+  projectId: "delta-cortex-387523",
+  storageBucket: "delta-cortex-387523.firebasestorage.app",
+  messagingSenderId: "489778411796",
+  appId: "1:489778411796:web:d76d850688f5cbdfa72a01",
+  measurementId: "G-XN4ZC5V0C8"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Conditionally initialize analytics only in the browser
+let analytics;
+if (typeof window !== 'undefined') {
+    analytics = getAnalytics(app);
+}
+
+
+export { app, analytics };
